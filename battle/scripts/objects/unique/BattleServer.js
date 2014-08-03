0daa81b0b87bec15d74d3090183d4c514d6eb9f4
varkor <github@varkor.com>
2014-08-03 16:53:09 +0100

    Made BattleServer an external object
    This makes BattleServer more compatible with the Node.js server.

--- battle/scripts/objects/unique/BattleServer.js
+++ battle/scripts/objects/unique/BattleServer.js
@@ -1,40 +1,40 @@
-BattleServer = {
+exports.BattleServer = {
 	clients : [],
 	battles = [],
 	receive : function (message, from) {
 		switch (message.action) {
 			case "connect":
-				BattleServer.clients.push(from);
+				exports.BattleServer.clients.push(from);
 				break;
 			case "invite":
 				if (message.hasOwnProperty("who")) {
 					var clientA, clientB = null;
 					clientA = from;
-					BattleServer.clients.forEach(function (client) {
+					exports.BattleServer.clients.forEach(function (client) {
 						if (client !== from && client.ip === message.who) {
 							clientB = client;
 							break;
 						}
 					});
 					if (clientB !== null) {
-						BattleServer.battles.push({
+						exports.BattleServer.battles.push({
 							clientA : clientA,
 							clientB : clientB,
 							seed : Math.random() * 10000000000000000
 						});
-						BattleServer.send({
+						exports.BattleServer.send({
 							action : "begin",
 							team : 0
 						}, clientA);
-						BattleServer.send({
+						exports.BattleServer.send({
 							action : "begin",
 							team : 1
 						}, clientB);
 					}
 				} else {
-					BattleServer.clients.forEach(function (client) {
+					exports.BattleServer.clients.forEach(function (client) {
 						if (client !== from)
-							BattleServer.send({
+							exports.BattleServer.send({
 								action : "invitation",
 								from : client
 							});
@@ -42,21 +42,24 @@ BattleServer = {
 				}
 				break;
 			case "actions":
-				BattleServer.battles.forEach(function (battle) {
+				exports.BattleServer.battles.forEach(function (battle) {
 					if (battle.clientA === from) {
-						BattleServer.send({
+						exports.BattleServer.send({
 							action : "actions",
 							actions : message.actions
 						}, clientB);
 						break;
 					} else if (battle.clientB === from) {
-						BattleServer.send({
+						exports.BattleServer.send({
 							action : "actions",
 							actions : message.actions
 						}, clientA);
 						break;
 					}
 				});
+				break;
+			default:
+				console.log("Received an unknown action from a client: ", message);
 				break;
 		}
 	},
