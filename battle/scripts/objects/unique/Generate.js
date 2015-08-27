86b5aa1ae8577a23b8b78cf9fea803138114af08
varkor <github@varkor.com>
2015-08-27 23:44:01 +0100

    Enabled the Battle Factory candidates to be set externally
    Separation of content from functionality is always good!

--- battle/scripts/objects/unique/Generate.js
+++ battle/scripts/objects/unique/Generate.js
@@ -1,27 +1,29 @@
 Generate = {
 	forSituation : {
-		BattleFactory : function (trainer) {
-			// Generates 6 Pokémon to use in the Battle Factory
-			var random = new srandom(trainer.identification);
-			var candidates = [];
-			if (candidates.notEmpty()) {
-				var lineup = [];
-				for (var i = 0; i < 6; ++ i) {
-					var species = random.chooseFromArray(candidates);
-					var moveset = Pokedex._(species).moveset;
-					var potentialMoves = [];
-					forevery(moveset, function (moves) {
-						potentialMoves = potentialMoves.concat(moves);
-					});
-					lineup.push(new pokemon({
-						species : species,
-						moves : random.chooseDistinctSelectionFromArray(potentialMoves, 4),
-						level : 100
-					}));
+		BattleFactory : {
+			candidates : [],
+			lineup : function (trainer) {
+				// Generates 6 Pokémon to use in the Battle Factory
+				var random = new srandom(trainer.identification);
+				if (Generate.forSituation.BattleFactory.candidates.notEmpty()) {
+					var lineup = [];
+					for (var i = 0; i < 6; ++ i) {
+						var species = random.chooseFromArray(Generate.forSituation.BattleFactory.candidates);
+						var moveset = Pokedex._(species).moveset;
+						var potentialMoves = [];
+						forevery(moveset, function (moves) {
+							potentialMoves = potentialMoves.concat(moves);
+						});
+						lineup.push(new pokemon({
+							species : species,
+							moves : random.chooseDistinctSelectionFromArray(potentialMoves, 4),
+							level : 100
+						}));
+					}
+					return lineup;
+				} else {
+					return null;
 				}
-				return lineup;
-			} else {
-				return null;
 			}
 		}
 	}
