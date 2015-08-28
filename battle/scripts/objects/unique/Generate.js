f697f41f972c81b61730f38e8c7a3658628c4ae9
varkor <github@varkor.com>
2015-08-28 22:17:31 +0100

    Enabled Generate.pokemon.forCompetition to batch-generate
    You can now either pass a single species, or an array (and will get
    either a single Pokémon or an array, respectively, in return).

--- battle/scripts/objects/unique/Generate.js
+++ battle/scripts/objects/unique/Generate.js
@@ -2,27 +2,36 @@ Generate = {
 	random : new srandom(),
 	pokemon : {
 		forCompetition : function (species, level) {
-			var moveset = Pokedex._(species).moveset;
-			var potentialMoves = [];
-			forevery(moveset, function (moves) {
-				potentialMoves = potentialMoves.concat(moves);
+			var competitors = [], multiple = Array.isArray(species);
+			species = wrapArray(species);
+			foreach(species, function (particularSpecies) {
+				var moveset = Pokedex._(particularSpecies).moveset;
+				var potentialMoves = [];
+				forevery(moveset, function (moves) {
+					potentialMoves = potentialMoves.concat(moves);
+				});
+				competitors.push(new pokemon({
+					species : particularSpecies,
+					moves : Generate.random.chooseDistinctSelectionFromArray(potentialMoves, 4),
+					level : arguments.length >- 2 ? level : 100
+				}));
 			});
-			return new pokemon({
-				species : species,
-				moves : Generate.random.chooseDistinctSelectionFromArray(potentialMoves, 4),
-				level : arguments.length >- 2 ? level : 100
-			});
+			if (multiple) {
+				return competitors;
+			} else {
+				return competitors.first();
+			}
 		}
 	},
 	species : {
 		fromSelection : function (number, selection, distinct) {
 			selection = selection.slice(0);
 			var species = [];
-			for (var i = 0; i < number; ++ number) {
+			for (var i = 0; i < number; ++ i) {
 				var index = Generate.random.int(selection.length);
 				species.push(selection[index]);
 				if (distinct) {
-					selection = selection.remove(index);
+					selection.remove(index);
 				}
 			}
 			return species;
