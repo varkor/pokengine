106bc14b37635c1134372df7a982e0c0b29613fb
varkor <github@varkor.com>
2015-09-03 20:43:09 +0100

    Added a minimised state to the party widget
    This is specifically for pokéngine. The code to switch between layouts
    is very… specific. FlowGrid was not meant to do anything like this. I’m
    not sure if it really ought to. This will do for now.

--- battle/scripts/objects/unique/Widgets.js
+++ battle/scripts/objects/unique/Widgets.js
@@ -12,7 +12,7 @@ Widgets.FlowGrid = {
 				for (var i = 0; i < states.length; ++ i) {
 					switch (states[i]) {
 						case "hover":
-							if (Widgets.Party.state.kind === "overworld" || (Widgets.Party.state.kind === "switch" && !poke.inBattle())) {
+							if (Widgets.Party.state.kind === "overworld") {
 								lightness += 5;
 							}
 							break;
@@ -74,6 +74,56 @@ Widgets.FlowGrid = {
 					context.fillTextHD("Send out", position.x + size.width / 2, position.y + size.height / 2);
 				}
 			}
+		}),
+		tinyPokemon : FlowCellTemplate({
+			size : {
+				width : 38,
+				height : 38
+			},
+			draw : function (context, poke, position, size, states) {
+				var lightness = 25;
+				for (var i = 0; i < states.length; ++ i) {
+					switch (states[i]) {
+						case "hover":
+							if (Widgets.Party.state.kind === "overworld") {
+								lightness += 5;
+							}
+							break;
+					}
+				}
+				context.fillStyle = "hsl(0, 0%, " + lightness + "%)";
+				context.fillRoundedRectHD(Math.round(position.x), Math.round(position.y), size.width, size.height, 4);
+				// Send-out banner
+				var clickedPoke = Display.original(poke);
+				if (Widgets.Party.state.kind === "switch" && states.contains("hover") && clickedPoke.trainer.healthyEligiblePokemon(true).contains(clickedPoke)) {
+					var bannerHeight = 32;
+					context.fillStyle = "hsl(0, 60%, 40%)";
+					context.fillRoundedRectHD(Math.round(position.x), Math.round(position.y), size.width, size.height, 4);
+					context.fillStyle = "hsl(0, 0%, 100%)";
+					context.textAlign = "center";
+					context.textBaseline = "bottom";
+					context.setFontHD("Arial", 8);
+					context.fillTextHD("Send", position.x + size.width / 2, position.y + size.height - 2);
+				} else {
+					// Health
+					var percentageHealth = poke.health / poke.maximumHealth();
+					context.fillStyle = (percentageHealth > 0.5 ? "hsl(110, 100%, 40%)" : percentageHealth > 0.2 ? "hsl(40, 100%, 50%)" : "hsl(0, 100%, 50%)");
+					context.fillRoundedRectHD(position.x + 3, position.y + size.height - 6, (percentageHealth * size.width - 6), 2, 1);
+				}
+				// Icon
+				var icon = Sprite.load(poke.paths.icon(true));
+				if (icon) {
+					context.imageSmoothingEnabled = false;
+					context.copyImageHD(icon.image, false, true, position.x + (size.width - icon.width) / 2, position.y + (size.height - icon.height) / 2 - 2);
+					context.imageSmoothingEnabled = true;
+				} else {
+					Sprite.load(poke.paths.icon(true), function () {
+						if (Widgets.Party.interface.cells.indexOf(poke) !== -1) {
+							Widgets.Party.interface.redrawCell(poke);
+						}
+					});
+				}
+			}
 		})
 	}
 };
@@ -82,20 +132,61 @@ Widgets.Party = {
 	state : {
 		kind : "overworld"
 	},
+	switchSize : function (maximise) {
+		var flowGrid = Widgets.Party.interface;
+		if (maximise) {
+			flowGrid.template = Widgets.FlowGrid.templates.pokemon;
+			flowGrid.rows = 2;
+			flowGrid.columns = 3;
+			flowGrid.margin = {
+				left : 8,
+				right : 8,
+				top : 8,
+				bottom : 8
+			};
+			flowGrid.spacing = {
+				x : 8,
+				y : 8
+			};
+		} else {
+			flowGrid.template = Widgets.FlowGrid.templates.tinyPokemon;
+			flowGrid.rows = 1;
+			flowGrid.columns = 6;
+			flowGrid.margin = {
+				left : 4,
+				right : 4,
+				top : 4,
+				bottom : 4
+			};
+			flowGrid.spacing = {
+				x : 4,
+				y : 4
+			};
+		}
+		flowGrid.size = {
+			width : flowGrid.columns * flowGrid.template.size.width + (flowGrid.columns - 1) * flowGrid.spacing.x + flowGrid.margin.left + flowGrid.margin.right,
+			height : flowGrid.rows * flowGrid.template.size.height + (flowGrid.rows - 1) * flowGrid.spacing.y + flowGrid.margin.top + flowGrid.margin.bottom,
+		};
+		flowGrid.canvas.width = flowGrid.size.width * window.devicePixelRatio;
+		flowGrid.canvas.height = flowGrid.size.height * window.devicePixelRatio;
+		flowGrid.canvas.style.width = flowGrid.size.width + "px";
+		flowGrid.canvas.style.height = flowGrid.size.height + "px";
+		flowGrid.refreshDataFromSource();
+	},
 	interface : FlowGrid({
-		template : Widgets.FlowGrid.templates.pokemon,
+		template : Widgets.FlowGrid.templates.tinyPokemon,
 		datasource : [],
 		rows : 1,
 		columns : 6,
 		contrainToBounds : true,
 		selection : "none",
 		margin : {
-			x : 8,
-			y : 8
+			x : 4,
+			y : 4
 		},
 		spacing : {
-			x : 8,
-			y : 8
+			x : 4,
+			y : 4
 		},
 		events : {
 			"cell:click" : function (index, poke) {
